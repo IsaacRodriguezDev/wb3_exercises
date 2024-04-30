b332ca9789054500123f0f0fc11772ede8cf3cd3
IsaacRodriguezDev <isaacrodriguez.dev@gmail.com>
2024-04-30 17:03:10 -0500

    fixes tax_functions.js

--- functions/tax_functions.js
+++ functions/tax_functions.js
@@ -3,42 +3,51 @@ function getSocSecTax(grossPay){
     let socialSecurityTax = grossPay*taxRate
     return socialSecurityTax
 }
-console.log(getSocSecTax(1000))
+console.log('Social security tax is: '+getSocSecTax(1000))
 
 function getMedicareTax(grossPay){
     let taxRate = 1.45/100
     let medicare = grossPay * taxRate
     return medicare
-
+   
 }
-console.log(getMedicareTax(800))
+console.log('Medicare is: '+getMedicareTax(800))
 
 function getFederalTax(grossPay,withHoldingCode){
     let federalTaxWithHolding 
     if(withHoldingCode === 0){
         let taxRate = 23/100
         federalTaxWithHolding = grossPay*taxRate
+        console.log(`This persons: gross pay of $${grossPay} and a withholding code of ${withHoldingCode} =`)
+
     }else if(withHoldingCode === 1){
         let taxRate = 21/100
          federalTaxWithHolding = grossPay*taxRate
+         console.log(`This persons: gross pay of $${grossPay} and a withholding code of ${withHoldingCode} =`)
+
     }else if(withHoldingCode ===2){
         let taxRate = 19.5/100
          federalTaxWithHolding = grossPay*taxRate
+         console.log(`This persons: gross pay of $${grossPay} and a withholding code of ${withHoldingCode} =`)
+
     }else if(withHoldingCode ===3){
         let taxRate = 18.5/100
          federalTaxWithHolding = grossPay*taxRate
-    }else if(withHoldingCode ===4){
+         console.log(`This persons: gross pay of $${grossPay} and a withholding code of ${withHoldingCode} =`)
+        }else if(withHoldingCode ===4){
          let taxRate = 18/100
           federalTaxWithHolding = grossPay*taxRate
+          console.log(`This persons: gross pay of $${grossPay} and a withholding code of ${withHoldingCode} =`)
+
          
          
          }else{
             
                 let codeAboveFour = withHoldingCode - 4
-                withHoldingCode =.18-(codeAboveFour*.005)
-                console.log(withHoldingCode)
-                let taxRate = withHoldingCode
+                let taxByCode = .18-(codeAboveFour*.005)
+                let taxRate = taxByCode
                 federalTaxWithHolding = grossPay*taxRate
+                console.log(`This persons: gross pay of $${grossPay} and a withholding code of ${withHoldingCode} =`)
                
          }
          return federalTaxWithHolding.toFixed(2)
@@ -47,5 +56,6 @@ function getFederalTax(grossPay,withHoldingCode){
     
 
 console.log(getFederalTax(750,0))
+
 console.log(getFederalTax(1550,2))
 console.log(getFederalTax(1100,6))
