07a8a59c9c777d28c007c03000d46670d88847f7
IsaacRodriguezDev <isaacrodriguez.dev@gmail.com>
2024-04-30 13:07:36 -0500

    completed simple_functions.js

---
+++ functions/simple_functions.js
@@ -0,0 +1,19 @@
+// function to display my name, favorite movie, and favorite band
+function favoriteThings(){
+    console.log(' My name is Isaac',' My favorite movie is Shrek','My favorite artist is Eve')
+}
+
+
+// function descrbing why you joined learn to code program
+function whyImHere(){
+    console.log('Joined to learn how to code and in the future hopefully get a job in this field')
+}
+
+// function describing favorite place to visit and why
+function favoritePlace(){
+    console.log('I would love to visit Japan because I love the culture and enjoy their fashion')
+}
+// calling/invoking the functions
+favoriteThings()
+whyImHere()
+favoritePlace()
